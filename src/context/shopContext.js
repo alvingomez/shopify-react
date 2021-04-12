efcdc8d6c451e0e1c26be8f51b0bc54c88ab5c47
Alvin Gomez <alvingomez.world@gmail.com>
2021-04-12 21:38:08 +0800

    Defined a variable and assigned createContext

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -3,6 +3,9 @@
 import React, { Component } from 'react';
 import Client from 'shopify-buy';
 
+const ShopContext = React.createContext
+
+
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
     domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
