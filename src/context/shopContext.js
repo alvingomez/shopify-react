ad98b29969b6b99a7c8dc300923b380c43ef270b
Alvin Gomez <alvingomez.world@gmail.com>
2021-04-12 19:06:29 +0800

    Created an environment for the Shopify domain and token. Initialize a client function by updating the domain and Access Token.

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -5,8 +5,8 @@ import Client from 'shopify-buy';
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-    domain: 'your-shop-name.myshopify.com',
-    storefrontAccessToken: 'your-storefront-access-token'
+    domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+    storefrontAccessToken: process.env.REACT_APP_SHOPIFY
   });
   
 
