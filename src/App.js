02dd9be923e8b711f28eb1539995c1e8066f321e
Alvin gomez <alvingomez.world@gmail.com>
2021-05-24 21:10:13 +0800

    React Router
    Created a react route for the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom'
+
 import Home from './pages/Home';
+import ProductPage from './pages/ProductPage';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 function App() {
   return (
     <div className="App">
-     <Home />
+     <Router> 
+       <Route path="/products/:handle" exact>
+        <ProductPage />
+       </Route>
+       <Route path="/">
+        <Home />
+       </Route> 
+     </Router>
     </div>
   );
 }
---
+++ src/pages/ProductPage.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useContext} from 'react';
+import {useParams, Link} from 'react-router-dom'
+
+import {ShopContext} from '../context/shopContext';
+
+const ProductPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductPage
