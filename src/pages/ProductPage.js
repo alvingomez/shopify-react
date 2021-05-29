c589a24696952a1560212d75adf4f5948c273e93
Alvin gomez <alvingomez.world@gmail.com>
2021-05-29 17:35:56 +0800

    UseEffect, useParams, and loading
    Added useParams handle to be used on useEffects.
    useEffect to load product chosen via handle as argument.

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -4,9 +4,21 @@ import {useParams, Link} from 'react-router-dom'
 import {ShopContext} from '../context/shopContext';
 
 const ProductPage = () => {
+    const {handle} = useParams();
+
+    const {fetchProductWithHandle, product} = useContext(ShopContext);
+
+    useEffect(() => {
+        fetchProductWithHandle(handle)
+    }, [fetchProductWithHandle, handle])
+
+    if(!product.title){
+        return <div>Loading...</div>
+    }
+        
     return (
         <div>
-            
+           <h1>{product.title}</h1> 
         </div>
     )
 }
