32faeeaaef3e8e3b6bd27d1cf2b9af24da974902
Alvin Gomez <alvingomez.world@gmail.com>
2021-06-16 20:54:48 +0800

    Responsive
    Used arrays as values for mobile-first responsive styles and used Flex for the image to align to center.

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -19,14 +19,16 @@ const ProductPage = () => {
         
     return (
         <Box>
-            <Grid templateColumns="repeat(2, 1fr)">
-                <Image src={product.images[0].src} />
-                <Box>
+            <Grid templateColumns={["repeat(1, 1fr)","repeat(2, 1fr)"]} m="auto">
+                <Flex justifyContent="center" alignItems="center"> 
+                    <Image src={product.images[0].src} />
+                </Flex>                
+                <Flex flexDirection="column">
                 <Text>{product.variants[0].price}</Text>
                 <Text>{product.description}</Text>
                 <Heading>{product.title}</Heading>
                 <Button onClick={() => addItemToCheckout(product.variants[0].id, 1)} >Add to Cart</Button>
-                </Box>
+                </Flex>
             </Grid>           
         </Box>
       
