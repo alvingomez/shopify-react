cb07f298e1f837fec3690ed3cc527b820213834e
Alvin Gomez <alvingomez.world@gmail.com>
2021-06-21 22:35:23 +0800

    RichText
    Created an extra text to be used for ad both above and below the images. Import the Richtext inside the homepage and position them to relative and absolute.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Hero from '../components/Hero';
 import ImageWithText from '../components/ImageWithText';
 
 import {ShopContext} from '../context/shopContext';
+import RichText from '../components/RichText';
 
 function Home() {
 
@@ -23,14 +24,18 @@ function Home() {
         
         <Box>
             <Hero />
+            <RichText 
+                heading="The relaxation you've been waiting for"
+                text="Our bath bombs guarantee a fun, relaxing, and colorful night."
+            />
             <Grid templateColumns={["repeat(1, 1fr)","repeat(3, 1fr)"]}>
                 {
                 products.map((product) => (
                     <Link to={`/products/${product.handle}`} key={product.id}>
-                        <Box _hover={{opacity:'80%'}} textAlign='center'>
+                        <Box _hover={{opacity:'80%'}} textAlign='center' position="relative">
                         <Image src={product.images[0].src}/>
-                            <Text>{product.title}</Text>
-                            <Text>
+                            <Text position="absolute" bottom="15%" w="100%" fontWeight="bold">{product.title}</Text>
+                            <Text position="absolute" bottom="5%" w="100%" color="gray.900" >
                              ${product.variants[0].price}
                             </Text>
                         </Box>                    
@@ -38,12 +43,16 @@ function Home() {
                     )   
                 }
             </Grid>
+            <RichText 
+                heading="Treat yourself"                
+            />
+            
             <ImageWithText  
              image="https:cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758" 
              heading="Heading"
              text="Splash is one of the family of fragrances created by Perfumer Gal at the turn of the 20th Century. 
              For about 100 years the  classic fragrance has been the most popular in Spain."
-             />
+             /> 
              <ImageWithText reverse 
              image="https:cdn.shopify.com/s/files/1/0472/5705/9496/files/bath-bomb-and-candle.jpg?v=1610066758" 
              heading="Heading2"
